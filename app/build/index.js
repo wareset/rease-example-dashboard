8996ee271ffeb4c7b7e0490a9b793942b84dccfd
rease <cftcview@gmail.com>
2022-09-20 02:49:30 +0500

    test

--- app/build/index.js
+++ app/build/index.js
@@ -1,1 +1,1 @@
-(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);System.register(['./ce0388ed.js'],(function(){'use strict';return{setters:[function(){}],execute:(function(){})}}));
+System.register(["./5bf6b54b.js"],(function(){"use strict";return{setters:[function(){}],execute:function(){}}}));
